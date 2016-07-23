50878251a964595c293ee4147afd68b5adf272cf
avo1 <voanhdung@gmail.com>
2016-07-23 13:00:55 +0700

    remove bridge .h file

--- BlueBird/MyColors.swift
+++ BlueBird/MyColors.swift
@@ -7,15 +7,16 @@
 //
 
 import Foundation
+import UIKit
 
 class MyColors {
-  static let bluesky = UIColor(red: 19/255, green: 158/255, blue: 236/255, alpha: 1)
-  static let carrot = UIColor(red: 250/255, green: 58/255, blue: 67/255, alpha: 1)
-  static let greenOfRetweetCount = UIColor(red: 19/255, green: 196/255, blue: 115/255, alpha: 1)
-  static let redOfFavCount = UIColor(red: 226/255, green: 22/255, blue: 62/255, alpha: 1)
-  
-  static let navigationTintColor = bluesky
-  static let selectedCellColor = UIColor(red: 1, green: 230/255, blue: 230/255, alpha: 1)
-  static let tableColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
-  
+    static let bluesky = UIColor(red: 19/255, green: 158/255, blue: 236/255, alpha: 1)
+    static let carrot = UIColor(red: 250/255, green: 58/255, blue: 67/255, alpha: 1)
+    static let greenOfRetweetCount = UIColor(red: 19/255, green: 196/255, blue: 115/255, alpha: 1)
+    static let redOfFavCount = UIColor(red: 226/255, green: 22/255, blue: 62/255, alpha: 1)
+    
+    static let navigationTintColor = bluesky
+    static let selectedCellColor = UIColor(red: 1, green: 230/255, blue: 230/255, alpha: 1)
+    static let tableColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    
 }
